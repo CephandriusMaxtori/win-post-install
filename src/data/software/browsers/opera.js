9e76fb62e9262bc10bd70f41699fe134b88eec80
Nolan <112007432+CephandriusMaxtori@users.noreply.github.com>
2026-06-09 18:58:54 -0400

    Add Opera browser configuration file

---
+++ src/data/software/browsers/opera.js
@@ -0,0 +1,11 @@
+export default {
+  "id": "opera",
+  "name": "Opera",
+  "description": "Fast, secure web browser with a built-in ad blocker and VPN",
+  "category": "browsers",
+  "wingetId": "Opera.Opera",
+  "icon": "OperaIcon",
+  "popular": true,
+  "requiresAdmin": false,
+  "license": "free"
+};
